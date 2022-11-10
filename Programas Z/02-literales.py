fc999000045a19ddcf0e466e681ad0ccc016eaa9
ElZetache <zetachewtf@gmail.com>
2022-11-10 21:33:20 +0100

    literales

--- Programas Z/02-literales.py
+++ Programas Z/02-literales.py
@@ -1,1 +1,13 @@
 #literales python
+print("2")
+print(2)
+#podemos escribir numeros hexadecimales(0x) o octales(0o)
+print(0x123)
+print(3e5)
+#tener en cuenta que cuanto python genera cadenas siempre busca la notacion mas corta, importante si se quieren generar literales
+print(0.0000000000000000000001)
+print("----------------------")
+print(True > False)
+print(True < False)
+print("----------------------")
+print("\"I'm\"\n\"\"learning\"\"\n\"\"\"Python\"\"\"")
