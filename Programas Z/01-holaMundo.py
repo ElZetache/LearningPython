86cdc1ba848d8b7bb171834643dd4bb4cdb0428b
ElZetache <zetachewtf@gmail.com>
2022-11-10 21:08:13 +0100

    subir
    subir

---
+++ Programas Z/01-holaMundo.py
@@ -0,0 +1,30 @@
+print("¡Hola, mundo!") 
+print("Albert Huerta Bernal")
+
+print("La Witsi Witsi Araña subió a su telaraña.")
+print()
+print("Vino la lluvia \ny se la llevó.")
+
+
+print("La Witsi Witsi Araña","subió","a su telaraña.")
+print("---------------------------------------------")
+#la funcion print por defecto genera un salto de linea al final, con el parametro end podemos cambiar 
+#lo que finaliza la frase, en el siguiente ejemplo simplemente substituimos el salto de linea por espacio.
+print("Mi nombre es ", end="")
+print("Monty Python.")
+#con el argumento sep= lo que podemos cambiar es como separa python los diferentes argumentos de la funcion.
+print("Mi", "nombre", "es", "Monty", "Python.", sep="-")
+print("---------------------------------------------")
+print("Programming","Essentials","in",sep="***",end="...")
+print("Python")
+
+print("---------------------------------------------")
+print("    *","\n   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
+
+
