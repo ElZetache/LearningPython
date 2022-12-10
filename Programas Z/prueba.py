add948dc7ceec6ad2444c8bdf24ea5bcb1f88bec
ElZetache <96047954+ElZetache@users.noreply.github.com>
2022-12-10 10:25:31 +0100

    ll

--- Programas Z/prueba.py
+++ Programas Z/prueba.py
@@ -22,3 +22,9 @@ def count_positives_sum_negatives(arr):
         if(num<=0): lista[1] = lista[1] + num
         else: lista[0] = lista[0] + 1
     return lista
+
+def century(year):
+    century = 0
+    if((year/100)>(int(year/100))): century = int(year/100) + 1
+    else: century = int(year/100)
+    return century
